c03b9d7594d9573b0021c813384c757034a254bd
BuildTools <unconfigured@null.spigotmc.org>
2025-02-10 14:57:03 +0100

    Footer has been made and has been made sticky to <body> also react-icons library has been installed

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="da">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}>
           <main>
             {children}
           </main>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { CiHome } from "react-icons/ci";
+import { IoSearchOutline } from "react-icons/io5";
+import { CiCalendar } from "react-icons/ci";
+
+export default function Footer() {
+    return (
+        <footer className="bg-white pt-3 pb-3 sticky bottom-0 w-full">
+            <nav>
+                <ul className="flex justify-around">
+                    <li className="border border-black rounded-full p-2">
+                        <Link href="/"><CiHome size={28} /></Link>
+                    </li>
+                    <li className="border border-black rounded-full p-2">
+                        <Link href="/"><IoSearchOutline size={28} /></Link>
+                    </li>
+                    <li className="border border-black rounded-full p-2">
+                        <Link href="/"><CiCalendar size={28} /></Link>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    );
+}
